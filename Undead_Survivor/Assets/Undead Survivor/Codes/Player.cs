abe3a752e31d73dc54bbafde70d3bacb847ad3be
gyuhyeonPark <gmfrwh12@gmail.com>
2025-02-24 21:08:22 +0900

    오브젝트 풀 적용

---
+++ Undead_Survivor/Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec = new Vector2(0f, 0f);
+    public float speed = 0f;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+            
+    }
+
+    void Update()
+    {
+    }
+    void FixedUpdate()
+    {
+        Vector2 speedVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + speedVec);
+        
+    }
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0f)
+            spriteRenderer.flipX = inputVec.x < 0f;
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+}
